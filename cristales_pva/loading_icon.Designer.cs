1a79825ac49c7bf858c892f8041f27b9c4259969
Isaac Solis <Ing_Isaacsolis@hotmail.com>
2019-04-08 12:46:00 -0600

    *Version 12 -- Patch 2.1.1

--- cristales_pva/loading_icon.Designer.cs
+++ cristales_pva/loading_icon.Designer.cs
@@ -83,7 +83,7 @@
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(12, 371);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 23);
